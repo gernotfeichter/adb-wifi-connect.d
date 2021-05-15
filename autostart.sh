d30bd368dfdbd00b06476770cea3058151dbd45a
Gernot Feichter <absolutanon@hotmail.com>
2021-05-15 08:27:07 +0200

    add logging

--- autostart.sh
+++ autostart.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-cd "${HOME}/.adb-wifi-connect.d" && source ./run.sh
+LOGFILE="${HOME}/.adb-wifi-connect.d/log"
+rm LOGFILE
+cd "${HOME}/.adb-wifi-connect.d" && source ./run.sh 2>&1 >"${LOGFILE}"
