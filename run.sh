2d15121188af467f6bb49368252756bad5e54741
Gernot Feichter <absolutanon@hotmail.com>
2021-05-16 15:13:30 +0200

    refactor: better rename this function to reflect what it actually does

--- run.sh
+++ run.sh
@@ -6,8 +6,7 @@ source ./config.sh
 
 function anyRequiredProcessRunning() {
   for process in ${REQUIRED_PROCESSES}; do
-    if pgrep -x "${process}" >/dev/null;
-    then
+    if pgrep -x "${process}" >/dev/null; then
       echo "true"
       return 0
     fi
@@ -15,10 +14,10 @@ function anyRequiredProcessRunning() {
   echo "false"
 }
 
-function deviceOnlineCheck() {
-  if [[ "${ONLY_SCAN_WHEN_NO_DEVICE_ONLINE}" = "true" ]]; then
+function deviceOfflineCheck() {
+  if [[ "${ONLY_SCAN_WHEN_NO_DEVICE_ONLINE}" == "true" ]]; then
     adb_devices_count="$(adb devices | grep device\$ | wc -l)"
-    [[ ${adb_devices_count} = 0 ]] && echo "true" && return 0
+    [[ ${adb_devices_count} == 0 ]] && echo "true" && return 0
     echo "false"
     return 0
   fi
@@ -27,10 +26,10 @@ function deviceOnlineCheck() {
 
 set +e
 while true; do
-  if [[ "$(anyRequiredProcessRunning)" = "true" ]] && [[ "$(deviceOnlineCheck)" = "true" ]]; then
+  if [[ "$(anyRequiredProcessRunning)" == "true" ]] && [[ "$(deviceOfflineCheck)" == "true" ]]; then
     nmap_output="$(nmap "${ANDROID_HOSTS}" -p "T:${ANDROID_PORTS}" --max-retries 0 --host-timeout "${ANDROID_HOSTS_TIMEOUT}")"
     echo "nmap_output=${nmap_output}"
-    while IFS= read -r line ; do
+    while IFS= read -r line; do
       if [[ "${line}" =~ ^Nmap\ scan\ report\ for.*\((.*)\)$ ]]; then
         current_host="${BASH_REMATCH[1]}"
       fi
@@ -39,7 +38,7 @@ while true; do
         adb start-server
         adb connect "${current_host}:${current_port}"
       fi
-  done <<< "${nmap_output}"
+    done <<<"${nmap_output}"
   fi
 
   sleep "${POLL_SLEEP_SECONDS}"
